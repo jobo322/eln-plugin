021dda0615b0385ba4d29bed3648b8e348c2c5eb
Daniel Kostro <kostro.d@gmail.com>
2016-03-14 13:40:33 +0100

    fix bulk require for build

--- src/types.js
+++ src/types.js
@@ -1,23 +1,22 @@
 'use strict';
 
 const bulk = require('bulk-require');
-const types = bulk(path.join(__dirname, 'types'), '*.js');
+const types = bulk(__dirname, 'types/*.js');
 
 
 module.exports = {
     getType(type, custom) {
-        return Object.assign({}, types.default, types[type], custom);
+        return Object.assign({}, lib.types.default, lib.types[type], custom);
     },
 
     getAllTypes(custom) {
         var all = [];
 
-        for(var type in types) {
+        for(var type in lib.types) {
             if(type !== 'default') {
                 all.push(module.exports.getType(type, custom));
             }
         }
-
         return all;
     }
 };
