893faaacbdc00704923cfc9e1689620c115a725f
Daniel Kostro <kostro.d@gmail.com>
2016-04-08 14:32:06 +0200

    refactor nmr parsing to nmr utils file

---
+++ src/types/nmr.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const jcampConverter = require('jcampconverter');
+
+var nmr = module.exports = {};
+
+nmr.getMetadata = function (filecontent) {
+        const metadata = {
+            nucleus: []
+        };
+
+        var jcamp = jcampConverter.convert(filecontent, {
+            keepRecordsRegExp: /.*/
+        });
+        var info = jcamp.info;
+        metadata.solvent = info['.SOLVENTNAME'];
+        metadata.pulse = info['.PULSESEQUENCE'] || info['.PULPROG'];
+        metadata.dimension = info['.NUMDIM'] || 1;
+        metadata.temperature = info['.TE'];
+        metadata.frequency = info['.OBSERVEFREQUENCY'];
+        metadata.title = info['TITLE'];
+
+        if(metadata.dimension === 1) {
+            var nucleus = info['.OBSERVENUCLEUS'];
+            if(nucleus) {
+                metadata.nucleus.push(nucleus);
+            }
+        } else {
+            nucleus = info['.NUCLEUS'];
+            if(nucleus) {
+                metadata.nucleus = metadata.nucleus.concat(nucleus.split(','));
+            }
+        }
+
+        return metadata;
+    };
--- src/types/product/nmr.js
+++ src/types/product/nmr.js
@@ -6,8 +6,7 @@ const replaceFid = /[^a-z]fid[^a-z]?/i;
 const replaceFt = /[^a-z]ft[^a-z]?/i;
 
 const common = require('../common');
-
-const jcampConverter = require('jcampconverter');
+const nmrLib = require('../nmr');
 
 
 module.exports = {
@@ -38,7 +37,7 @@ module.exports = {
     process(filename, content) {
         const extension = common.getExtension(filename);
         if(extension === 'jdx' || extension === 'dx') {
-            return getNmrMetadata(content);
+            return nmrLib.getMetadata(content);
         }
         return {};
     },
@@ -63,40 +62,3 @@ function getReference(filename) {
     return reference;
 }
 
-function getNmrMetadata(filecontent) {
-    const metadata = {
-        nucleus: []
-    };
-
-    var jcamp = jcampConverter.convert(filecontent, {
-        keepRecordsRegExp: /.*/
-    });
-    var info = jcamp.info;
-    metadata.solvent = info['.SOLVENTNAME'];
-    metadata.pulse = info['.PULSESEQUENCE'] || info['.PULPROG'];
-    metadata.dimension = info['.NUMDIM'] || 1;
-    metadata.temperature = info['.TE'];
-    metadata.frequency = info['.OBSERVEFREQUENCY'];
-    metadata.title = info['TITLE'];
-
-    if(metadata.dimension === 1) {
-        var nucleus = info['.OBSERVENUCLEUS'];
-        if(nucleus) {
-            metadata.nucleus.push(nucleus);
-        }
-    } else {
-        nucleus = info['.NUCLEUS'];
-        if(nucleus) {
-            metadata.nucleus = metadata.nucleus.concat(nucleus.split(','));
-        }
-    }
-
-    return metadata;
-}
-
-function getLineIfExist(str, prefix) {
-    const line = str.indexOf(prefix);
-    if (line > -1) {
-        return str.substring(line + prefix.length, str.indexOf('\n', line)).trim();
-    }
-}
