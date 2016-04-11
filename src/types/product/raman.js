a20818b0893ee080f700fc5873eda96ddf03affa
Michaël Zasso <targos@protonmail.com>
2016-04-11 17:17:07 +0200

    add raman type

---
+++ src/types/product/raman.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+    jpath: ['spectra', 'raman'],
+    find: common.basenameFind,
+    getProperty: common.jcampGetProperty
+};
