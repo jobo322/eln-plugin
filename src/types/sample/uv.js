c12aa5bcbe5d27528c43924aedb701116249c516
Luc Patiny <luc.patiny@epfl.ch>
2018-06-19 18:26:14 +0200

    add uv in sample

---
+++ src/types/sample/uv.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+    jpath: ['spectra', 'uv'],
+    find: common.basenameFind,
+    getProperty: common.getTargetProperty
+};
