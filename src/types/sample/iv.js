6d9da40bb63be699a87c6c824a020d356d4d020b
Luc Patiny <luc@patiny.com>
2018-06-25 16:51:06 +0200

    add iv

---
+++ src/types/sample/iv.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+    jpath: ['spectra', 'iv'],
+    find: common.basenameFind,
+    getProperty: common.getTargetProperty
+};
