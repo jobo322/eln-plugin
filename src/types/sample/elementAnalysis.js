655324c11cb826d753cebda162a3bef0f1518d58
Luc Patiny <luc@patiny.com>
2018-12-05 08:44:12 +0100

    add elementAnalysis

---
+++ src/types/sample/elementAnalysis.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+  jpath: ['spectra', 'elementalAnalysis'],
+  find: common.basenameFind,
+  getProperty: common.getTargetProperty
+};
