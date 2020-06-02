9f4740eeb7fe7c0356cef675909d03f93892831b
Luc Patiny <luc@patiny.com>
2020-06-02 16:18:26 +0200

    feat: add xrd and xps

---
+++ src/types/sample/xps.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+  jpath: ['spectra', 'xps'],
+  find: common.basenameFind,
+  getProperty: common.getTargetProperty,
+};
---
+++ src/types/sample/xrd.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+  jpath: ['spectra', 'xrd'],
+  find: common.basenameFind,
+  getProperty: common.getTargetProperty,
+};
