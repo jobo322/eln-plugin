a543281d27a755296b5c2b533c6b5d7cdf3168a7
Luc Patiny <luc@patiny.com>
2021-02-07 09:16:11 +0100

    feat: add differentialCentrifugalSedimentation

---
+++ src/types/sample/differentialCentrifugalSedimentation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+  jpath: ['spectra', 'differentialCentrifugalSedimentation'],
+  find: common.basenameFind,
+  getProperty: common.getTargetProperty
+};
